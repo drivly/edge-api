96d0b60d2898bdca62eb45371713e8cde0f68322
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2023-06-24 03:59:48 -0500

    Cached user

--- jwt/withUser.js
+++ jwt/withUser.js
@@ -1,11 +1,19 @@
 import { decode } from 'next-auth/jwt'
 import { withCookies } from 'itty-router'
 
+const cookies = {}
+
 export const withUser = async (req, env, ctx) => {
   withCookies(req)
-  const { name, email, image } = await decode({
-    token: req.cookies['__Secure-next-auth.session-token'],
-    secret: env.JWT_SECRET,
-  }) || {}
-  req.user = { name, email, image }
+  if (cookies[req.cookies['__Secure-next-auth.session-token']]) {
+    req.user = cookies[req.cookies['__Secure-next-auth.session-token']]
+  } else {
+    const { name, email, image } = await decode({
+      token: req.cookies['__Secure-next-auth.session-token'],
+      secret: env.JWT_SECRET,
+    }) || {}
+    req.user = { name, email, image }
+    cookies[req.cookies['__Secure-next-auth.session-token']] = req.user
+  }
+  console.info('withUser', req.user)
 }
