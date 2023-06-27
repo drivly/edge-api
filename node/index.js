fdf492e56d05c84795547336118efc94024e825d
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2023-06-27 02:22:50 -0500

    Added node service support

--- node/index.js
+++ node/index.js
@@ -1,1 +1,13 @@
 export * from '@whatwg-node/server'
+
+import { createServerAdapter } from '@whatwg-node/server'
+import { createServer } from 'http'
+
+export const server = (api, port = 3000) => {
+  const service = createServerAdapter(
+    (req, env, ctx) => api.fetch(req, env, ctx)
+  )
+  const httpServer = createServer(service)
+  httpServer.listen(port)
+}
+
