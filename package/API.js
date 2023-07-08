c457576ee0b119493426cda2d0f30bda4178a44f
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2023-07-08 06:26:48 -0500

    Reverted auto otel

--- package/API.js
+++ package/API.js
@@ -1,6 +1,5 @@
 import { error, Router, withParams, withContent } from 'itty-router'
 import { Toucan } from 'toucan-js'
-import { instrument } from '@microlabs/otel-cf-workers'
 import { json } from './json.js'
 import { withUrl } from './middleware/index.js'
 
@@ -18,17 +17,9 @@ export const API = options => {
       context: ctx,
     })
 
-    const honeycomb = {
-      exporter: {
-        url: 'https://api.honeycomb.io/v1/traces',
-        headers: { 'x-honeycomb-team': env.HONEYCOMB_TEAM },
-      },
-      // service: { name: 'greetings' },
-    }
-
     try {
       const startTime = Date.now()
-      const data = env.HONEYCOMB_TEAM ? await instrument(api.handle(req, env, ctx), honeycomb) : await api.handle(req, env, ctx)
+      const data = await api.handle(req, env, ctx)
       const responseTime = Date.now() - startTime
       const response = json(data)
       // response.headers.set('X-Response-Time', `${responseTime}ms`)
