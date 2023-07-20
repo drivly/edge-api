05d65c2c8b2ebbdf79998209c0e4e452cb0bcdd5
Nathan Clevenger <nateclev@gmail.com>
2023-07-20 06:18:36 -0500

    added api metadata

--- package/API.js
+++ package/API.js
@@ -5,14 +5,15 @@ import { withContext, withUrl } from './middleware/index.js'
 import { isResponse } from './utils/isResponse.js'
 
 export const API = options => {
-  const api = Router(options)
+  const { domain, description, site, repo, type, from, prices, dsn, base, routes } = options
+  const api = Router({ base, routes })
   api
     .all('*', withParams, withContent, withUrl, withContext)
 
   api.fetch = async (req, env, ctx) => {
 
     const sentry = new Toucan({
-      dsn: env.SENTRY_DSN,
+      dsn: env.SENTRY_DSN ?? dsn,
       request: req,
       // release: '1.0.0',
       context: ctx,
@@ -22,13 +23,26 @@ export const API = options => {
       const startTime = Date.now()
       const data = await api.handle(req, env, ctx)
       const responseTime = Date.now() - startTime
-      const { user } = req
+      const { origin, hostname, user } = req
       user.serviceLatency = responseTime
       // const response = json(data)
+      const base = domain ? `https://${domain}`.toLowerCase() : origin
+      const metadata = {
+        name: domain ?? hostname,
+        description,
+        url: base,
+        login: `${base}/login`,
+        signup: `${base}/signup`,
+        subscribe: prices ? `${base}/subscribe` : undefined,
+        site,
+        repo,
+        type,
+        from,
+      }
       const response = isResponse(data) ? data : 
         Array.isArray(data) 
-          ? json({ '@context': req.origin, data, user })
-          : json({ '@context': req.origin, ...data, user })
+          ? json({ api: metadata, data, user })
+          : json({ api: metadata, ...data, user })
       // response.headers.set('X-Response-Time', `${responseTime}ms`)
       // TODO: add logging
       return response
