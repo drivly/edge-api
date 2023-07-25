b1721bf7fbf698832f414c43e5dae9eb9fc1ec9e
Nathan Clevenger <nateclev@gmail.com>
2023-07-25 04:34:19 -0500

    Fixed export

--- package/middleware/withWorkers.js
+++ package/middleware/withWorkers.js
@@ -1,4 +1,4 @@
-export const withUrl = workers => (req, env, ctx) => {
+export const withWorkers = workers => (req, env, ctx) => {
   const worker = req.headers.get('cf-worker')
   if (worker && (workers == worker || workers.includes(worker))) {
     req.user = { name: worker, email: 'cf-worker@' + worker, ...req.user }
