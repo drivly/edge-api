a00e9c3802643d28750dc739e2f8edd76c92d9e2
Nathan Clevenger <nateclev@gmail.com>
2023-07-25 05:21:31 -0500

    Fixed worker user override edge case

--- package/middleware/withWorkers.js
+++ package/middleware/withWorkers.js
@@ -2,6 +2,6 @@ export const withWorkers = workers => (req, env, ctx) => {
   const worker = req.headers.get('cf-worker')
   const workerIp = req.headers.get('cf-connecting-ip') == '2a06:98c0:3600::103'
   if (worker && workerIp && (workers == worker || workers.includes(worker))) {
-    req.user = { name: worker, email: 'cf-worker@' + worker, ...req.user }
+    req.user = { name: req.user?.name ?? worker, email: req.user?.email ??'cf-worker@' + worker, ...req.user }
   }
 }
