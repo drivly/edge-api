31f2cfe16e0d4947d8f8142cd28928de7c928428
Nathan Clevenger <nateclev@gmail.com>
2023-07-25 04:58:24 -0500

    Added worker IP

--- package/middleware/withWorkers.js
+++ package/middleware/withWorkers.js
@@ -1,6 +1,7 @@
 export const withWorkers = workers => (req, env, ctx) => {
   const worker = req.headers.get('cf-worker')
-  if (worker && (workers == worker || workers.includes(worker))) {
+  const workerIp = req.headers.get('cf-connecting-ip') == '2a06:98c0:3600::103'
+  if (worker && workerIp && (workers == worker || workers.includes(worker))) {
     req.user = { name: worker, email: 'cf-worker@' + worker, ...req.user }
   }
 }
