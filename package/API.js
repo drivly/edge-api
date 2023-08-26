856bc604f65ffaee25fbb86cca7303fe2b55e14f
Nathan Clevenger <nateclev@gmail.com>
2023-08-26 11:58:55 -0500

    Changed log settings sequence

--- package/API.js
+++ package/API.js
@@ -38,7 +38,7 @@ export const API = (options = {}) => {
 
     try {
       const startTime = Date.now()
-      if (logBody || env.LOG_BODY) req.logBody = true
+      if (env.LOG_BODY || logBody) req.logBody = true
       const data = await api.handle(req, env, ctx)
       const responseTime = Date.now() - startTime
       const { origin, hostname, user, query } = req
