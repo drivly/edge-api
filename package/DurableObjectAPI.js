53456a6a7d3a58330047bafeef8e2b2300ef0b4e
Nathan Clevenger <nateclev@gmail.com>
2023-09-01 05:53:14 -0500

    Added DO Stub

---
+++ package/DurableObjectAPI.js
@@ -0,0 +1,3 @@
+// TODO: Figure out how to expose an interface to the worker and the DO
+
+// TODO: Define a mechanism for mapping the 'ID' of a DO to a specific instance, hostname, path, segment, subdomain, etc
