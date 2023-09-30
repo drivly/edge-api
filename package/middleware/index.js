f3054e7d09ad37d090d63ab6a0f74d8676164c91
Nathan Clevenger <nateclev@gmail.com>
2023-09-30 06:26:00 -0500

    Missed export

--- package/middleware/index.js
+++ package/middleware/index.js
@@ -1,4 +1,5 @@
-export * from './assertUser.js'
 export * from './withContext.js'
+export * from './withContent.js'
 export * from './withUrl.js'
+export * from './assertUser.js'
 export * from './withWorkers.js'
