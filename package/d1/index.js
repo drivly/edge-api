6a0473788236afebcb4259ff57e53dd20036aaee
Nathan Clevenger <nateclev@gmail.com>
2023-10-02 03:25:18 -0500

    Fixed more d1 bugs

--- package/d1/index.js
+++ package/d1/index.js
@@ -31,7 +31,7 @@ export const DB = (db) => {
       return {
         list: async ({ order = 'id', offset = 0, limit = 100 } = {}) => {
           const statement = `select * from data where type = ? order by ? limit ? offset ?`
-          const results = await db.prepare(statement).all(type, order, limit, offset)
+          const results = await db.prepare(statement).bind(type, order, limit, offset).all()
           return results
         },
         find: async (query, { order = 'id', offset = 0, limit = 100 } = {}) => {
@@ -42,57 +42,57 @@ export const DB = (db) => {
         },
         findOne: async (query) => {
           const statement = `select * from data where type = ? ${Object.keys(query).map(key => ` and data->'$.${key}' = ?`).join('')} limit 1`
-          const results = await db.prepare(statement).bind(type, ...Object.values(query)).get()
+          const results = await db.prepare(statement).bind(type, ...Object.values(query)).all()
           return results
         },
         get: async (id) => {
           const statement = `select * from data where type = ? and id = ?`
-          const results = await db.prepare(statement).get(type, id)
+          const results = await db.prepare(statement).bind(type, id).all()
           return results
         },
         set: async (id, data) => {
           const statement = `update data set data = json_patch(data, ?) where type = ? and id = ?`
-          const results = await db.prepare(statement).run(JSON.stringify(data), type, id)
+          const results = await db.prepare(statement).bind(JSON.stringify(data), type, id).run()
           return results
         },
         overwrite: async (id, data) => {
           const statement = `update data set data = ? where type = ? and id = ?`
-          const results = await db.prepare(statement).run(JSON.stringify(data), type, id)
+          const results = await db.prepare(statement).bind(JSON.stringify(data), type, id).run()
           return results
         },
         delete: async (id) => {
           const statement = `delete from data where type = ? and id = ?`
-          const results = await db.prepare(statement).run(type, id)
+          const results = await db.prepare(statement).bind(type, id).run()
           return results
         },
         insert: async (id, data) => {
           const statement = `insert into data (type, id, data) values (?, ?, ?)`
-          const results = await db.prepare(statement).run(type, id, JSON.stringify(data))
+          const results = await db.prepare(statement).bind(type, id, JSON.stringify(data)).run()
           return results
         },
         insertMany: async (data, { id } = {}) => {
           const statement = `insert into data (type, id, data) values ${data.map(({ id }) => `(?, ?, ?)`).join(',')}`
-          const results = await db.prepare(statement).run(...data.flatMap(data => [type, id ? data[id] : (data._id ?? data.id ?? base62(8)), JSON.stringify(data)]))
+          const results = await db.prepare(statement).bind(...data.flatMap(data => [type, id ? data[id] : (data._id ?? data.id ?? base62(8)), JSON.stringify(data)])).run()
           return results
         },
         upsert: async (id, data) => {
           const statement = `insert into data (type, id, data) values (?, ?, ?) on conflict (type, id) do update set data = json_patch(data, ?)`
-          const results = await db.prepare(statement).run(type, id, JSON.stringify(data), JSON.stringify(data))
+          const results = await db.prepare(statement).bind(type, id, JSON.stringify(data), JSON.stringify(data)).run()
           return results
         },
         upsertMany: async (data, { id } = {}) => {
           const statement = `insert into data (type, id, data) values ${data.map(({ id }) => `(?, ?, ?)`).join(',')} on conflict (type, id) do update set data = excluded.data`
-          const results = await db.prepare(statement).run(...data.flatMap(data => [type, id ? data[id] : (data._id ?? data.id ?? base62(8)), JSON.stringify(data)]))
+          const results = await db.prepare(statement).bind(...data.flatMap(data => [type, id ? data[id] : (data._id ?? data.id ?? base62(8)), JSON.stringify(data)])).run()
           return results
         },
         update: async (id, data) => {
           const statement = `update data set data = json_patch(data, ?) where type = ? and id = ?`
-          const results = await db.prepare(statement).run(JSON.stringify(data), type, id)
+          const results = await db.prepare(statement).bind(JSON.stringify(data), type, id).run()
           return results
         },
         count: async (query) => {
           const statement = `select count(*) from data where type = ? ${Object.keys(query).map(key => ` and data->'$.${key}' = ?`).join('')}`
-          const results = await db.prepare(statement).get(type, ...Object.values(query))
+          const results = await db.prepare(statement).bind(type, ...Object.values(query)).all()
           return results['count(*)']
         },
       }
